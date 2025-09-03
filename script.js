22aa45ed6a0d4ba45a8fa99b3d031630dd0bc3f7
FERNANDO GABRIEL JACONI DA SILVA <nando06>
2025-09-03 10:50:37 -0300

    exercicios iniciantes prontos

---
+++ script.js
@@ -0,0 +1,43 @@
+//*Nivel Basico = 1*//
+// let Idade = parseInt(prompt("Digite sua idade: "))
+
+// if (Idade >= 18) {
+//     console.log("Maior de idade")
+// } else {
+//     console.log("menor de idade")
+// }
+
+//* Nivel Basico = 2*//
+
+// let Numero = parseInt(prompt("Digite um numero: "))
+
+// if (Numero <= 0) {
+//     console.log("Numero negativo")
+// } else {
+//     console.log("Numero positivo")
+// }
+
+//* Nivel Basico = 3*//
+
+// let Nota = parseInt(prompt("Digite a nota: "))
+
+// if(Nota >= 60){
+//     console.log("Aprovado")
+// }else{
+//     console.log("Reprovado")
+// }
+
+//* Nivel Basico = 4*//
+
+// let Numero = parseInt(prompt("Digite o numero: "))
+
+// if(Numero >=1){
+//     console.log("Numero positivo")
+// }else if (Numero == 0 ){
+//     console.log("0")
+// }else{
+//     console.log("Numero negativo")
+// }
+
+//* Nivel Basico = 5*//
+
